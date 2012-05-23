4cba9ba13e633d4eeeb8b9551e8864e76699dc2d
Andrew Ace <web@andrewace.com>
2012-05-23 19:26:01 +0100

    infopages

--- js/employeelist.js
+++ js/employeelist.js
@@ -12,6 +12,11 @@ $("#categoryPage").bind("pageinit", function (e) {
 
 });
 
+$("#schoolInfoPage").bind("pageinit", function (e) {
+
+	getInfoList();
+
+});
 
 function getCategoryList() {
 
@@ -37,6 +42,36 @@ function getCategoryList() {
 		$("#categoryList a.ui-link-inherit").css("padding-bottom",elempadding+"px");
 		
 		$("#categoryList div.ui-btn-inner").css("height",elemheight+"px");
+		
+		
+
+	});
+} 
+
+function getInfoList() {
+
+	$.getJSON(siteURL + apiPath + 'getinfopages.php?', function(data) {
+		$('#schoolInfoList li').remove();
+		infoPages = data.items;
+		$.each(infoPages, function(index, schoolInfo) {
+			$('#schoolInfoList').append('<li><a href="schoolInfodetails.html?id=' + schoolInfo.slug + '">' +
+					'<h4>' + schoolInfo.name + '</h4></a></li>');
+		});
+
+		$('#schoolInfoList').listview('refresh');
+		$(".ui-page div.ui-content").iscrollview()
+		
+		var numelements=$(".ui-link-inherit").length
+		var boxheight=$(".ui-page div.ui-content").height()		
+		var elemheight=Math.round(boxheight/numelements);
+		var elempadding=Math.round((elemheight-40)/2);
+		if (elempadding<0) { elempadding=0; }
+		
+		
+		$("#schoolInfoList a.ui-link-inherit").css("padding-top",elempadding+"px");
+		$("#schoolInfoList a.ui-link-inherit").css("padding-bottom",elempadding+"px");
+		
+		$("#schoolInfoList div.ui-btn-inner").css("height",elemheight+"px");
 		
 		
 
