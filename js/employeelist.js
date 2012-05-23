5055e60ae117eedab8b1ecc49e88fd47e3f0c22e
Andrew Ace <web@andrewace.com>
2012-05-24 00:57:17 +0100

    pages

--- js/employeelist.js
+++ js/employeelist.js
@@ -4,19 +4,8 @@ var imgPath = "wp-content/uploads/";
 
 
 var categories;
+var infoPages;
 
-
-$("#categoryPage").bind("pageinit", function (e) {
-
-	getCategoryList();
-
-});
-
-$("#schoolInfoPage").bind("pageinit", function (e) {
-
-	getInfoList();
-
-});
 
 function getCategoryList() {
 
@@ -29,20 +18,18 @@ function getCategoryList() {
 		});
 
 		$('#categoryList').listview('refresh');
-		$(".ui-page div.ui-content").iscrollview()
+		$(".ui-page div.ui-content").iscrollview();
 		
-		var numelements=$(".ui-link-inherit").length
-		var boxheight=$(".ui-page div.ui-content").height()		
+		var numelements=$("#categoryList .ui-link-inherit").length;
+		var boxheight=$(".ui-page div.ui-content").height();
+		
 		var elemheight=Math.round(boxheight/numelements);
 		var elempadding=Math.round((elemheight-40)/2);
 		if (elempadding<0) { elempadding=0; }
 		
-		
 		$("#categoryList a.ui-link-inherit").css("padding-top",elempadding+"px");
 		$("#categoryList a.ui-link-inherit").css("padding-bottom",elempadding+"px");
-		
 		$("#categoryList div.ui-btn-inner").css("height",elemheight+"px");
-		
 		
 
 	});
@@ -50,29 +37,32 @@ function getCategoryList() {
 
 function getInfoList() {
 
-	$.getJSON(siteURL + apiPath + 'getinfopages.php?', function(data) {
+
+	$.getJSON(siteURL + apiPath + 'getinfolist.php?', function(data) {
 		$('#schoolInfoList li').remove();
 		infoPages = data.items;
-		$.each(infoPages, function(index, schoolInfo) {
-			$('#schoolInfoList').append('<li><a href="schoolInfodetails.html?id=' + schoolInfo.slug + '">' +
-					'<h4>' + schoolInfo.name + '</h4></a></li>');
+				
+		$.each(infoPages, function(index, page) {
+			$('#schoolInfoList').append('<li><a href="schoolInfodetails.html?id=' + page.ID + '">' +
+					'<h4>' + page.post_title + '</h4></a></li>');
 		});
 
+		$('#schoolInfoList').append('<li><a href="sports.html"><h4>Sports &amp; Clubs</h4></a></li>');
+		$('#schoolInfoList').append('<li><a href="events.html"><h4>Fundraising &amp; Events</h4></a></li>');
+
 		$('#schoolInfoList').listview('refresh');
-		$(".ui-page div.ui-content").iscrollview()
+		$(".ui-page div.ui-content").iscrollview();
 		
-		var numelements=$(".ui-link-inherit").length
-		var boxheight=$(".ui-page div.ui-content").height()		
+		var numelements=$("#schoolInfoList .ui-link-inherit").length;
+		var boxheight=$("div.ui-content").height();	
+		
 		var elemheight=Math.round(boxheight/numelements);
 		var elempadding=Math.round((elemheight-40)/2);
 		if (elempadding<0) { elempadding=0; }
 		
-		
 		$("#schoolInfoList a.ui-link-inherit").css("padding-top",elempadding+"px");
 		$("#schoolInfoList a.ui-link-inherit").css("padding-bottom",elempadding+"px");
-		
-		$("#schoolInfoList div.ui-btn-inner").css("height",elemheight+"px");
-		
+		$("#schoolInfoList div.ui-btn-inner").css("height",elemheight+"px");		
 		
 
 	});
