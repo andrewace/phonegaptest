c904dd9a92bcbc7e2dd6f2014f0581dfe06d8d24
Andrew Ace <web@andrewace.com>
2012-05-23 17:13:31 +0100

    .

--- js/employeelist.js
+++ js/employeelist.js
@@ -20,12 +20,25 @@ function getCategoryList() {
 		categories = data.items;
 		$.each(categories, function(index, category) {
 			$('#categoryList').append('<li><a href="categorydetails.html?id=' + category.slug + '">' +
-					'<img src="' + siteURL + imgPath + category.slug + '.jpg"/>' +
 					'<h4>' + category.name + '</h4></a></li>');
 		});
 
 		$('#categoryList').listview('refresh');
 		$(".ui-page div.ui-content").iscrollview()
+		
+		var numelements=$(".ui-link-inherit").length
+		var boxheight=$(".ui-page div.ui-content").height()		
+		var elemheight=Math.round(boxheight/numelements);
+		var elempadding=Math.round((elemheight-40)/2);
+		if (elempadding<0) { elempadding=0; }
+		
+		
+		$("#categoryList a.ui-link-inherit").css("padding-top",elempadding+"px");
+		$("#categoryList a.ui-link-inherit").css("padding-bottom",elempadding+"px");
+		
+		$("#categoryList div.ui-btn-inner").css("height",elemheight+"px");
+		
+		
 
 	});
 } 
