bb2d04a1ab9cd0adbab48ba092fa8164642424a8
Andrew Ace <web@andrewace.com>
2012-06-11 00:25:52 +0100

    working>

--- js/app.js
+++ js/app.js
@@ -61,7 +61,7 @@ function getCategoryList() {
 		});
 
 		$('#categoryList').listview('refresh');
-		$(".ui-page div.ui-content").iscrollview();
+		$("#categoryPage div.ui-content").iscrollview();
 		
 		var numelements=$("#categoryList .ui-link-inherit").length;
 		var boxheight=$(".ui-page div.ui-content").height();
@@ -192,16 +192,20 @@ function getSinglePost(data) {
 		$('.ui-page-active #postImage').html(thepost.image); 
 		$('.ui-page-active #gallery').html(thepost.gallery); 
 		$('.ui-page-active #attachments').html(thepost.attachments); 
-		$(".ui-page-active div.ui-content").iscrollview();
 
 	});
-	
+	 
 	$(".ui-page-active #loadingmsg").remove();
 	$(".ui-page-active #singleContent").attr('style', 'display:block');
 	
 	if ($('.ui-page-active #gallery').html()!="") {
 		photoSwipeInstance = $("div.imggallery a", theTarget).photoSwipe(options,  currentPage.attr('pagename'));
 	}	
+
+
+	$("#postViewPage div.ui-content").attr('id', 'thePageID');
+
+	$("#thePageID").iscrollview();
 
 }
 
